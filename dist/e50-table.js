399607d254ab2837604d7fd697dfd37a593671e6
Will Durney <wdurney@wesleyan.edu>
2014-12-10 15:38:03 -0500

    Add initial loading message

--- dist/e50-table.js
+++ dist/e50-table.js
@@ -202,6 +202,9 @@ angular.module('e50Table').directive('e50IfData', function () {
       var show = attrs.e50IfData !== 'false';
       var replace = attrs.e50IfNoData ? attrs.e50IfNoData : null;
       var $none = angular.element('<div class="e50-no-data">'+replace+'</div>');
+      var loading = 'e50IfLoadingData' in attrs ?
+        (attrs.e50IfLoadingData.length ? attrs.e50IfLoadingData : 'Loading data') : null;
+      var $noneL = angular.element('<div class="e50-no-data">'+loading+'</div>');
 
       // Hide & show the element based on data status
       scope.$watchCollection('e50GetData()', function(v) {
@@ -210,10 +213,16 @@ angular.module('e50Table').directive('e50IfData', function () {
           if (replace) {
             $none.remove();
           }
+          if (loading) {
+            $noneL.remove();
+          }
           element.removeClass('ng-hide');
         } else {
-          if (replace) {
+          if (replace && typeof data !== 'undefined') {
             element.parent()[0].insertBefore($none[0], element[0]);
+          }
+          if (loading && typeof data === 'undefined') {
+            element.parent()[0].insertBefore($noneL[0], element[0]);
           }
           element.addClass('ng-hide');
         }

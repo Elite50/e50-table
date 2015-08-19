811dcffe9bf9f832be5d0e7252a3f7448d661818
Will Durney <willdurney@gmail.com>
2015-08-19 14:08:40 -0400

    Fix issues with e50-view

--- src/directives/e50-view.js
+++ src/directives/e50-view.js
@@ -5,16 +5,22 @@ angular.module('e50Table').directive('e50View', function ($timeout) {
     link: function (scope, element, attrs, ctrl) {
 
       // Hide & show the element based on current view
-      ctrl.$attrs.$observe('e50Views', function(v) {
+      function changeView(v) {
         if (v === attrs.e50View) {
           element.removeClass('ng-hide');
           $timeout(function() {
-            scope.e50InfiniteScroll(true);
+            if (scope.e50InfiniteScroll) {
+              scope.e50InfiniteScroll(true);
+            }
           });
         } else {
           element.addClass('ng-hide');
         }
-      });
+      }
+
+      // Watch for view changes
+      changeView(ctrl.$attrs.e50Views);
+      ctrl.$attrs.$observe('e50Views', changeView);
 
     }
   };

2392568320a9c1027859d6e21c2bc6da29781872
Will Durney <willdurney@gmail.com>
2015-11-11 12:36:07 -0500

    Add task to use table in another project during development

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,6 +26,19 @@ module.exports = function(grunt) {
         },
         files: ['demo/*.css', 'src/**/*.js', 'demo/*.html', 'demo/*.js', 'Gruntfile.js'],
         tasks: ['default']
+      },
+      use: {
+        options: {
+          livereload: 2342
+        },
+        files: ['demo/*.css', 'src/**/*.js', 'demo/*.html', 'demo/*.js', 'Gruntfile.js'],
+        tasks: ['use']
+      }
+    },
+    copy: {
+      use: {
+        src: 'dist/*',
+        dest: grunt.option('dest')
       }
     },
     bump: {
@@ -40,6 +53,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-ng-annotate');
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-bump');
 
   grunt.registerTask('default', [
@@ -50,5 +64,15 @@ module.exports = function(grunt) {
   grunt.registerTask('default:watch', [
     'default',
     'watch'
+  ]);
+
+  grunt.registerTask('use', [
+    'default',
+    'copy:use'
+  ]);
+
+  grunt.registerTask('use:watch', [
+    'use',
+    'watch:use'
   ]);
 };
